88ce03171b2297e7a9c9826216a32716b01dfa64
Udit <udit.singh98@gmail.com>
2021-05-29 21:40:49 +0530

    Just trying something

--- Django_practice/Django_practice/settings.py
+++ Django_practice/Django_practice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5u)7fu#07ukcx8&!ghu6nvcp&k6j9ja25iax277)*s9pnj8l#8'
+SECRET_KEY = '1z)(bt0qamc+q*f0uc5&z68a#6n(pebp(x%*-d&+kqn!_&8_5p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
